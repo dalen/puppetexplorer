8d6408733837aa7fb08aaa3b3d8c9580cad19576
Erik Dalén <erik.gustav.dalen@gmail.com>
2017-02-04 14:40:58 +0100

    Upgrade to webpack 2

--- webpack.config.js
+++ webpack.config.js
@@ -16,65 +16,85 @@ module.exports = {
     filename: 'bundle.js',
   },
   resolve: {
-    extensions: ['', '.js', '.jsx'],
+    extensions: ['.js', '.jsx'],
   },
   // use imports loader to add whatwg-fetch polyfill
   plugins: [
     new webpack.ProvidePlugin({
-      fetch: 'imports?this=>global!exports?global.fetch!whatwg-fetch',
+      fetch: 'imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch',
     }),
   ],
   module: {
-    loaders: [
-      {
-        include: /\.json$/,
-        loaders: ['json-loader'],
-      },
+    rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel',
-        query: {
-          presets: ['react'], // ['es2015', 'react'] for release build
-          plugins: [
-            'babel-plugin-transform-es2015-modules-commonjs', // Full es2015 preset instead on release
-            'transform-class-properties',
-          ],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react'], // ['es2015', 'react'] for release build
+            plugins: [
+              'babel-plugin-transform-es2015-modules-commonjs', // Full es2015 preset instead on release
+              'transform-class-properties',
+            ],
+          },
         },
       },
       {
         test: /\.html$/,
-        loaders: [
-          'file?name=[name].[ext]',
-          'extract',
-          'html?attrs=img:src link:href',
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+            },
+          },
+          {
+            loader: 'extract-loader',
+          },
+          {
+            loader: 'html-loader',
+            options: {
+              attrs: 'img:src',
+              link: 'href',
+            },
+          },
         ],
       },
       {
         test: /\.css$/,
-        loaders: [
-          'file',
-          'extract',
-          'css',
+        use: [
+          'file-loader',
+          'extract-loader',
+          'css-loader',
         ],
       },
       {
         test: /\.(svg|ttf|woff|woff2|eot)$/,
-        loader: 'file',
+        use: 'file-loader',
       },
       {
         test: /\.ico$/,
-        loader: 'file?name=[name].[ext]',
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+          },
+        },
       },
       {
         test: /config\.js\.example$/,
-        loader: 'file?name=[name].[ext]',
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+          },
+        },
       },
     ],
   },
   devServer: {
     port: 4080,
-    colors: true,
     inline: true,
     historyApiFallback: {
       disableDotRule: true, // certnames usually contain dots
