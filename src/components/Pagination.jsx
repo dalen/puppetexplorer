ea6f637e6d2b711e2879c2e193f6ca051d64e614
Erik Dalén <erik.gustav.dalen@gmail.com>
2017-06-28 15:18:43 +0200

    Fix flow error

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -5,30 +5,26 @@ import { Pagination as RBPagination } from 'react-bootstrap';
 // Wrap react-bootstrap Pagination and calculate number of pages and
 // if we should show pagination at all automatically
 const Pagination = (props: {
-    count: ?number,
-    perPage: number,
-    activePage: number,
-    onSelect: (page: number) => void,
-  }): ?React$Element<*> => {
-  const numPages = Math.ceil(props.count / props.perPage);
+  count: ?number,
+  perPage: number,
+  activePage: number,
+  onSelect: (page: number) => void,
+}): ?React$Element<*> => {
+  const numPages = props.count == null ? 1 : Math.ceil(props.count / props.perPage);
 
-  return ((numPages === 1) ?
-      null
-    :
-      ((props.count) ?
-        <RBPagination
-          first
-          prev={numPages > 2}
-          next={numPages > 2}
-          last
-          items={Math.ceil(numPages)}
-          activePage={props.activePage}
-          onSelect={props.onSelect}
-        />
-      :
-        null
-    )
-  );
+  return numPages === 1
+    ? null
+    : props.count
+      ? <RBPagination
+        first
+        prev={numPages > 2}
+        next={numPages > 2}
+        last
+        items={Math.ceil(numPages)}
+        activePage={props.activePage}
+        onSelect={props.onSelect}
+      />
+      : null;
 };
 
 export default Pagination;
