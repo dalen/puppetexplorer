34fd24d0c87d8030475695c43f8343d895a54dcf
Erik Dalén <erik.gustav.dalen@gmail.com>
2017-07-27 23:41:09 +0200

    Lint fixes

--- src/modules/events/components/Events.jsx
+++ src/modules/events/components/Events.jsx
@@ -8,52 +8,42 @@ import paginatedList from '../../../components/PaginatedList';
 
 const PaginatedEventList = paginatedList(EventList, 'events', 'events');
 
-export default class Events extends React.Component {
-  props: {
-    serverUrl: string,
-    queryParsed: ?queryT,
-  };
-
-  render() {
-    return (
-      <div>
-        <Grid fluid>
-          <Row>
-            <Col md={4}>
-              <EventChart
-                title="Containing Class"
-                eventField="containing_class"
-                id="containing-class"
-                serverUrl={this.props.serverUrl}
-                queryParsed={this.props.queryParsed}
-              />
-            </Col>
-            <Col md={4}>
-              <EventChart
-                title="Resource Type"
-                eventField="resource_type"
-                id="resource-type"
-                serverUrl={this.props.serverUrl}
-                queryParsed={this.props.queryParsed}
-              />
-            </Col>
-            <Col md={4}>
-              <EventChart
-                title="Resource Status"
-                eventField="status"
-                id="resource-status"
-                serverUrl={this.props.serverUrl}
-                queryParsed={this.props.queryParsed}
-              />
-            </Col>
-          </Row>
-        </Grid>
-        <PaginatedEventList
-          serverUrl={this.props.serverUrl}
-          listQuery={this.props.queryParsed}
-          countQuery={['extract', [['function', 'count']], this.props.queryParsed]}
-        />
-      </div>
-    );
-  }
-}
+export default (props: { serverUrl: string, queryParsed: ?queryT }) =>
+  (<div>
+    <Grid fluid>
+      <Row>
+        <Col md={4}>
+          <EventChart
+            title="Containing Class"
+            eventField="containing_class"
+            id="containing-class"
+            serverUrl={props.serverUrl}
+            queryParsed={props.queryParsed}
+          />
+        </Col>
+        <Col md={4}>
+          <EventChart
+            title="Resource Type"
+            eventField="resource_type"
+            id="resource-type"
+            serverUrl={props.serverUrl}
+            queryParsed={props.queryParsed}
+          />
+        </Col>
+        <Col md={4}>
+          <EventChart
+            title="Resource Status"
+            eventField="status"
+            id="resource-status"
+            serverUrl={props.serverUrl}
+            queryParsed={props.queryParsed}
+          />
+        </Col>
+      </Row>
+    </Grid>
+    <PaginatedEventList
+      serverUrl={props.serverUrl}
+      listQuery={props.queryParsed}
+      countQuery={['extract', [['function', 'count']], props.queryParsed]}
+    />
+  </div>);
--- src/modules/facts/components/Facts.jsx
+++ src/modules/facts/components/Facts.jsx
@@ -8,40 +8,33 @@ import FactChart from './FactChart';
 import FactTree from '../FactTree';
 
 // The facts view
-export default class Facts extends React.Component {
-  props: {
-    serverUrl: string,
-    queryParsed: ?queryT,
-    factTree: FactTree,
-    activeFactCharts: OrderedSet<factPathT>,
-    toggleChart: (chart: factPathT) => void,
-    factSelect: (fact: factPathT, value: string) => void,
-  };
-
-  render() {
-    return (
-      <Grid>
-        <Row>
-          <Col md={6}>
-            <FactList
-              factTree={this.props.factTree}
-              activeFactCharts={this.props.activeFactCharts}
-              toggleChart={this.props.toggleChart}
-            />
-          </Col>
-          <Col md={6}>
-            {this.props.activeFactCharts.map(fact =>
-              (<FactChart
-                fact={fact}
-                serverUrl={this.props.serverUrl}
-                queryParsed={this.props.queryParsed}
-                key={fact.join('.')}
-                onSelect={value => this.props.factSelect(fact, value)}
-              />),
-            )}
-          </Col>
-        </Row>
-      </Grid>
-    );
-  }
-}
+export default (props: {
+  serverUrl: string,
+  queryParsed: ?queryT,
+  factTree: FactTree,
+  activeFactCharts: OrderedSet<factPathT>,
+  toggleChart: (chart: factPathT) => void,
+  factSelect: (fact: factPathT, value: string) => void,
+}) =>
+  (<Grid>
+    <Row>
+      <Col md={6}>
+        <FactList
+          factTree={props.factTree}
+          activeFactCharts={props.activeFactCharts}
+          toggleChart={props.toggleChart}
+        />
+      </Col>
+      <Col md={6}>
+        {props.activeFactCharts.map(fact =>
+          (<FactChart
+            fact={fact}
+            serverUrl={props.serverUrl}
+            queryParsed={props.queryParsed}
+            key={fact.join('.')}
+            onSelect={value => props.factSelect(fact, value)}
+          />),
+        )}
+      </Col>
+    </Row>
+  </Grid>);
