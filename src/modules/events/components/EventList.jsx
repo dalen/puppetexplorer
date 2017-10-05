02199b8710d2ad0b11022957ea8f51cf3c64c265
Erik Dalén <erik.gustav.dalen@gmail.com>
2017-10-05 14:57:11 +0200

    Use stateless function instead

--- src/modules/events/components/EventList.jsx
+++ src/modules/events/components/EventList.jsx
@@ -4,39 +4,25 @@ import { Alert, Table } from 'react-bootstrap';
 
 import EventListItem from './EventListItem';
 
-type Props = {
-  events: ?(eventT[]),
-  showNode: boolean,
-};
-
-export default class EventList extends React.Component<Props> {
-  static defaultProps = {
-    events: [],
-    showNode: true,
-  };
-
-  render() {
-    if (this.props.events) {
-      return (
-        <Table hover>
-          <thead>
-            <tr>
-              {this.props.showNode && <th>Node</th>}
-              <th>Resource</th>
-              <th>Status</th>
-              <th>Property</th>
-              <th>From</th>
-              <th>To</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.props.events.map((event, i) =>
-              <EventListItem event={event} showNode={this.props.showNode} key={i} />,
-            )}
-          </tbody>
-        </Table>
-      );
-    }
-    return <Alert bsStyle="warning">No events found</Alert>;
+export default ({ events = [], showNode = true }: { events: eventT[], showNode: boolean }) => {
+  if (events) {
+    return (
+      <Table hover>
+        <thead>
+          <tr>
+            {showNode && <th>Node</th>}
+            <th>Resource</th>
+            <th>Status</th>
+            <th>Property</th>
+            <th>From</th>
+            <th>To</th>
+          </tr>
+        </thead>
+        <tbody>
+          {events.map((event, i) => <EventListItem event={event} showNode={showNode} key={i} />)}
+        </tbody>
+      </Table>
+    );
   }
-}
+  return <Alert bsStyle="warning">No events found</Alert>;
+};
