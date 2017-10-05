0d1b793dc89f49a8a2919aa1acdc3c98664e7a14
Erik Dalén <erik.gustav.dalen@gmail.com>
2017-10-05 15:14:31 +0200

    use a function for loglistitem

--- src/modules/reports/components/LogListItem.jsx
+++ src/modules/reports/components/LogListItem.jsx
@@ -3,76 +3,64 @@ import React from 'react';
 import { Label } from 'react-bootstrap';
 import Moment from 'react-moment';
 
-type Props = {
-  log: logT,
+// Return the CSS class event states should correspond to
+const color = (status: string): string => {
+  switch (status) {
+    case 'crit':
+      return 'danger';
+    case 'emerg':
+      return 'danger';
+    case 'alert':
+      return 'danger';
+    case 'err':
+      return 'danger';
+    case 'warning':
+      return 'warning';
+    case 'notice':
+      return 'success';
+    case 'info':
+      return 'info';
+    case 'verbose':
+      return 'info';
+    case 'debug':
+      return 'default';
+    default:
+      return '';
+  }
 };
 
-export default class LogListItem extends React.Component<Props> {
-  // Return the CSS class event states should correspond to
-  static color(status: string): string {
-    switch (status) {
-      case 'crit':
-        return 'danger';
-      case 'emerg':
-        return 'danger';
-      case 'alert':
-        return 'danger';
-      case 'err':
-        return 'danger';
-      case 'warning':
-        return 'warning';
-      case 'notice':
-        return 'success';
-      case 'info':
-        return 'info';
-      case 'verbose':
-        return 'info';
-      case 'debug':
-        return 'default';
-      default:
-        return '';
-    }
+const message = (msg: string) => {
+  if (/\r|\n/.exec(msg)) {
+    return <pre>{msg}</pre>;
   }
+  return <p>message</p>;
+};
 
-  static message(message: string) {
-    if (/\r|\n/.exec(message)) {
-      return (
-        <pre>
-          {message}
-        </pre>
-      );
-    }
-    return <p>message</p>;
-  }
-
-  render() {
-    const log = this.props.log;
-    return (
-      <tr>
-        <td>
-          <Moment format="LLL" title={log.time}>
-            {log.time}
-          </Moment>
-        </td>
-        <td>
-          <Label bsStyle={LogListItem.color(log.level)} style={{ textTransform: 'capitalize' }}>
-            {log.level}
-          </Label>
-        </td>
-        <td>
-          {LogListItem.message(log.message)}
-          <span>
-            <strong>Source:</strong>&nbsp;{log.source}
-          </span>
-          {log.file &&
-            log.line &&
-            <p>
-              <strong>File:</strong>&nbsp;{log.file}
-              <strong>:</strong>
-              {log.line}
-            </p>}
-        </td>
-      </tr>
-    );
-  }
-}
+export default ({ log }: { log: logT }) => (
+  <tr>
+    <td>
+      <Moment format="LLL" title={log.time}>
+        {log.time}
+      </Moment>
+    </td>
+    <td>
+      <Label bsStyle={color(log.level)} style={{ textTransform: 'capitalize' }}>
+        {log.level}
+      </Label>
+    </td>
+    <td>
+      {message(log.message)}
+      <span>
+        <strong>Source:</strong>&nbsp;{log.source}
+      </span>
+      {log.file &&
+        log.line && (
+          <p>
+            <strong>File:</strong>&nbsp;{log.file}
+            <strong>:</strong>
+            {log.line}
+          </p>
+        )}
+    </td>
+  </tr>
+);
